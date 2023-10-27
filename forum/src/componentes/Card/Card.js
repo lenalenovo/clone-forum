1e30d64b94588a825fdf87916e504d411afc3d53
Helena Vasconcelos <hc065522@gmail.com>
2023-10-27 09:36:27 -0300

    helena

--- forum/src/componentes/Card/Card.js
+++ forum/src/componentes/Card/Card.js
@@ -5,12 +5,50 @@ import Comentar from '../Comentar/Comentar';
 import { GlobalStateContect } from '../../GlobalState/GlobalStateContext';
 
 function Card() {
+  const [loading, setLoading] = useState(true)
+  const [forumTopics, setForumTopics] = useState([])
 
+  const {selectedPostId} = useContext(GlobalStateContect)
+
+  useEffect(()=>{
+    getPostAll(setForumTopics)
+},[])
  
   return (
     <>
 
-   
+   {
+
+    loading?(
+  <ContainerCard>
+  {forumTopics && forumTopics.map(dado=>{
+  return(
+      <CardStyle key={dado.post.id}>
+           <PerfilUsuario>
+              <ImgCard src={'https://s2.glbimg.com/czDZS5koMVp_0qBUfiw_SdBlHCA=/s.glbimg.com/jo/g1/f/original/2016/04/08/fat.png'}/>
+              <ContainerPerfil>
+                  <NomeCard>{dado.creator_username}</NomeCard>
+                  <MensagemCard>{dado.post_created_at}</MensagemCard>
+              </ContainerPerfil>
+          </PerfilUsuario>
+          <TituloCard>{dado.post_title}</TituloCard>
+          <CardPost>
+            <ImgPost src={dado.post_image} alt='foto post'/>
+            <ConteudoCard>{dado.post_content}</ConteudoCard>
+          </CardPost>
+          <EditPost>
+            <Comentar
+              postId={dado.post_id}
+              comments={dado.comments}
+              autorId= {dado.created_id}
+            />
+          </EditPost>
+      </CardStyle>
+)
+})}
+</ContainerCard>):(<p>loading</p>)
+
+    }
 
 
     </>
--- forum/src/pages/CriarPost/CriarPost.js
+++ forum/src/pages/CriarPost/CriarPost.js
@@ -8,8 +8,27 @@ import { useVerificarToken } from "../../hooks/useverificarToken";
 
 const CriarPost = () => {
  
+  const [title, setTitle] = useState('')
+  const [content, setContent]= useState('')
+  const [image, setImage] = useState("")
+  const [hashtag, setHashtag]= useState("")
 
+  const criarPostApi = async(e)=>{
+    e.preventDefault()
+    if(!title || !content){
+      alert("Título e conteúdo são campos obrigatórios.")
+    }
+    const hashtagsArray = hashtag.split(",")
 
+    await createPost(title, content, image, hashtagsArray)
+    .then((response)=>{
+      console.log("Post criado com sucesso!", response)
+})  .catch((error)=>{
+      console.error("Erro ao criar o post:", error)
+})
+}
+
+  useVerificarToken()
   return (
    <>
    criarpost
